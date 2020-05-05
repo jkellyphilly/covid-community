12eb7137bb793d84c0f5c7a7f842467a65e67d90
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-05 11:12:15 -0400

    basic volunteer signup

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -11,6 +11,35 @@ class VolunteerController < ApplicationController
     end
   end
 
+  get '/volunteers/signup' do
+    if !is_logged_in?(session)
+      erb :'volunteers/signup'
+    else
+      # TODO: add flash message that you're already logged in
+      redirect "/deliveries"
+    end
+  end
+
+  post '/volunteers/signup' do
+    if username_already_taken?(params[:volunteer][:username])
+      # TODO: add flash message that this username is already taken
+      session[:message] = "The username you entered is already being used by another volunteer in our database. Please enter a new username."
+      redirect "/volunteers/signup"
+    else
+      @volunteer = Volunteer.new(params[:volunteer])
+
+      if @volunteer.save
+        session[:user_type] = "volunteer"
+        session[:user_id] = @volunteer.id
+        redirect "/deliveries"
+      else
+        # TODO: add in flash message that all fields must be filled out correctly
+        session[:message] = "Error: all fields must be filled out in order to sign up. Please try again."
+        redirect "/volunteers/signup"
+      end
+    end
+  end
+
   # --- HELPER METHODS --- #
 
   helpers do
@@ -20,7 +49,7 @@ class VolunteerController < ApplicationController
     end
 
     def username_already_taken?(username)
-      !!Member.find_by(username: username)
+      !!Volunteer.find_by(username: username)
     end
 
   end
--- app/models/volunteers.rb
+++ app/models/volunteers.rb
@@ -3,4 +3,9 @@ class Volunteer < ActiveRecord::Base
 
   has_many :deliveries
   has_many :members, through: :deliveries
+
+  validates :username, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
 end
