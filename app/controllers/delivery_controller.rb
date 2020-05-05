82488ddc59a36954329ac0595ab2d9fda36c79c4
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-05 15:02:06 -0400

    added more logic for deliveries index

--- app/controllers/delivery_controller.rb
+++ app/controllers/delivery_controller.rb
@@ -1,10 +1,33 @@
 class DeliveryController < ApplicationController
 
   get '/deliveries' do
-    @new_requests = Delivery.new_requests
-    @confirmed_requests = Delivery.confirmed_requests
-    @completed_requests = Delivery.completed_requests
-    erb :'deliveries/index'
+    if is_logged_in?(session)
+      @new_requests = Delivery.new_requests
+      @confirmed_requests = Delivery.confirmed_requests
+      @completed_requests = Delivery.completed_requests
+      erb :'deliveries/index'
+    else
+      redirect "/"
+    end
+  end
+
+  get '/deliveries/new' do
+    if session[:user_type] == "member"
+      erb :'deliveries/new'
+    else
+      session[:message] = "You must be logged in as a community member to create a new delivery request."
+      redirect "/deliveries"
+    end
+  end
+
+  # --- HELPER METHODS --- #
+
+  helpers do
+
+    def is_logged_in?(session)
+      !!session[:user_id]
+    end
+
   end
 
 end
