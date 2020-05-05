de7ed4d8d4fc78b56d2a93c33ec1f6342a0ce29f
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-05 11:02:22 -0400

    basic volunteer index

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -1,7 +1,28 @@
 class VolunteerController < ApplicationController
 
   get '/volunteers' do
-    "Print full list of volunteers"
+    if is_logged_in?(session)
+      @all_volunteers = Volunteer.all
+      erb :'volunteers/index'
+    else
+      # TODO: add flash message that you're not logged in
+      # customize to members
+      redirect "/volunteers/login"
+    end
+  end
+
+  # --- HELPER METHODS --- #
+
+  helpers do
+
+    def is_logged_in?(session)
+      !!session[:user_id]
+    end
+
+    def username_already_taken?(username)
+      !!Member.find_by(username: username)
+    end
+
   end
 
 end
