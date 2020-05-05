4ed0ff372aa0752156fad8c7e7dd3ad6b5df4545
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-04 20:44:49 -0400

    environment set up

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    "Hello World!"
+  end
+
+end
---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
+
+require_all 'app'
