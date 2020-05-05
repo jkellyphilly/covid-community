5b5d6ae4f74ae372a82d6e503ae70351fd78d6fb
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-05 14:46:14 -0400

    enhanced functionality for deliveries index page

--- app/controllers/delivery_controller.rb
+++ app/controllers/delivery_controller.rb
@@ -1,6 +1,9 @@
 class DeliveryController < ApplicationController
 
   get '/deliveries' do
+    @new_requests = Delivery.new_requests
+    @confirmed_requests = Delivery.confirmed_requests
+    @completed_requests = Delivery.completed_requests
     erb :'deliveries/index'
   end
 
--- app/models/deliveries.rb
+++ app/models/deliveries.rb
@@ -3,4 +3,16 @@ class Delivery < ActiveRecord::Base
   belongs_to :member
   belongs_to :volunteer
 
+  def self.new_requests
+    Delivery.all.select {|delivery| delivery.status == "new" && !delivery.volunteer}
+  end
+
+  def self.confirmed_requests
+    Delivery.all.select {|delivery| delivery.status == "confirmed" && !!delivery.volunteer}
+  end
+
+  def self.completed_requests
+    Delivery.all.select {|delivery| delivery.status == "completed" && !!delivery.volunteer}
+  end
+
 end
