b894cb2d59653500edc02c523664254bd7c6cf34
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-30 18:12:11 -0400

    configuring database specifications for prod push (Heroku)

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,24 @@
-ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require
 
-ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
+configure :development do
+  ENV['SINATRA_ENV'] ||= "development"
 
-require_all 'app'
+  require 'bundler/setup'
+  Bundler.require(:default, ENV['SINATRA_ENV'])
+
+  ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
